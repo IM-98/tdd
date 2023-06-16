16552e8516838c0af003f7a9eea4b8c07d2171ca
IM-98 <108485107+IM-98@users.noreply.github.com>
2023-06-16 10:27:47 +0200

    typo

--- src/main/java/com/example/tdd/entity/ClientEntity.java
+++ src/main/java/com/example/tdd/entity/ClientEntity.java
@@ -3,6 +3,7 @@ package com.example.tdd.entity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
--- src/main/java/com/example/tdd/repository/ClientRepository.java
+++ src/main/java/com/example/tdd/repository/ClientRepository.java
@@ -4,7 +4,6 @@ import com.example.tdd.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
